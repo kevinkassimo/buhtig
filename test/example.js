1594872652951c24f42b63d111261d6b3d28d2a6
kevinkassimo <kevinkassimo@gmail.com>
2017-02-22 14:41:18 -0800

    Tidying Code and Add Page Footer
    As shown in above…

--- test/example.js
+++ test/example.js
@@ -1,7 +1,7 @@
 var requestor = require('../lib/req.js');
 
-requestor.getRepoCommitCount('expressjs/express', x => console.log(x));
-requestor.getNthCommitURL('expressjs/express', 5308, x => console.log(x));
-requestor.getNthCommitURL('expressjs/express', 5309, x => console.log(x));
-requestor.getNthCommitURL('kevinkassimo/SEASHelper', 1, x => console.log(x));
-requestor.getNthCommitURL('somethingStinks/Stinky', 1, x => console.log(x));
+requestor.getRepoCommitCount('expressjs/express', x => console.log(x), x => console.log('ERR: '+x));
+requestor.getNthCommitURL('expressjs/express', 5308, x => console.log(x), x => console.log('ERR: '+x));
+requestor.getNthCommitURL('expressjs/express', 5309, x => console.log(x), x => console.log('ERR: '+x));
+requestor.getNthCommitURL('kevinkassimo/SEASHelper', 1, x => console.log(x), x => console.log('ERR: '+x));
+requestor.getNthCommitURL('somethingStinks/Stinky', 1, x => console.log(x), x => console.log('ERR: '+x));
---
+++ test/validtest.js
@@ -0,0 +1,6 @@
+var requestor = require('../lib/req.js');
+
+requestor.getRepoCommitCount('expressjs/express', x => console.log(x), x => console.log('ERR: '+x));
+requestor.getNthCommitURL('expressjs/express', 1, x => console.log(x), x => console.log('ERR: '+x));
+requestor.getNthCommitURL('expressjs/express', 5308, x => console.log(x), x => console.log('ERR: '+x));
+requestor.getNthCommitURL('kevinkassimo/SEASHelper', 1, x => console.log(x), x => console.log('ERR: '+x));

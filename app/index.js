5e545c2e8d242c560a711cc62839af16d6d9cec1
kevinkassimo <kevinkassimo@gmail.com>
2017-02-22 01:37:45 -0800

    Update Prototype App
    Basic goto n-th commit function implemented

--- app/index.js
+++ app/index.js
@@ -4,16 +4,30 @@ var url = require('url');
 var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {
-	fs.readFile(__dirname + '/index.html', function (err, data) {
-		if (err) {
-			res.writeHead(500, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin':'http://192.241.229.194:100/'});
-			res.end('500 - Internal Error');
-		} else {
-			res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin':'http://192.241.229.194:100/'});
-			res.write(data);
-			res.end();
-		}
-	});
+	var message = url.parse(req.url).pathname;
+	if (message.substring(0, 6) === '/fonts') {
+		fs.readFile(__dirname + message, function (err, data) {
+			if (err) {
+				res.writeHead(500, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin':'*'});
+				res.end('500 - Internal Error');
+			} else {
+				res.writeHead(200, {'Content-Type': 'application/octet-stream', 'Access-Control-Allow-Origin':'*'});
+				res.write(data);
+				res.end();
+			}
+		});
+	} else {
+		fs.readFile(__dirname + '/index.html', function (err, data) {
+			if (err) {
+				res.writeHead(500, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin':'http://192.241.229.194:100/'});
+				res.end('500 - Internal Error');
+			} else {
+				res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin':'http://192.241.229.194:100/'});
+				res.write(data);
+				res.end();
+			}
+		});
+	}
 }).listen(80);
 
 console.log('Server started; press Ctrl-C to terminate.');
--- app/server.js
+++ app/server.js
@@ -15,18 +15,18 @@ var server = http.createServer(function(req, res) {
 	
 	if (message['repo'] && message['commit']) {
 		let returnCallback = function(link) {
-			res.writeHead(200, {'content-type': 'text/plain', 'Access-Control-Allow-Origin':'http://192.241.229.194'});
+			res.writeHead(200, {'content-type': 'text/plain', 'Access-Control-Allow-Origin':'http://www.buhtig.com'});
 			res.write('lnk:' + link);
 			res.end();
 		}
 		let errorHandler = function(err) {
-			res.writeHead(500, {'content-type': 'text/plain', 'Access-Control-Allow-Origin':'http://192.241.229.194'});
+			res.writeHead(500, {'content-type': 'text/plain', 'Access-Control-Allow-Origin':'http://www.buhtig.com'});
 			res.write('err:' + err);
 			res.end();
 		}
 		reqlib.getNthCommitURL(message['repo'], parseInt(message['commit'], 10), returnCallback, errorHandler);
 	} else {
-		res.writeHead(404, {'content-type': 'text/plain', 'Access-Control-Allow-Origin':'http://192.241.229.194'});
+		res.writeHead(404, {'content-type': 'text/plain', 'Access-Control-Allow-Origin':'http://www.buhtig.com'});
 		res.write('404 - Not Found');
 		res.end();
 	}
